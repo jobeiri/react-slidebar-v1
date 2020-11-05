3aa152b27f0e5533d91bd28fb075fb3cbd08d586
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-11-05 03:54:46 -0800

    Adding react routes to display page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Reports from "./components/pages/Reports";
 
 function App() {
   return (
@@ -8,7 +11,9 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/reports" component={Reports} />
         </Switch>
       </BrowserRouter>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import { IconContext } from "react-icons";
 import { SlidebarData } from "./SlidebarData";
+import "./Navbar.css";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -11,30 +13,32 @@ function Navbar() {
 
   return (
     <>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-          <FaIcons.FaBars className="FaBars" onClick={showSidebar} />
-        </Link>
-      </div>
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toggle">
-            <Link to="#" className="menu-bars">
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SlidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.className}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars className="FaBars" onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SlidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.className}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
